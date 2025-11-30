a502e16f47ba7e036a75d6ac403e053709fc92fa
bogosorter <luiswbarbosa@gmail.com>
2025-11-30 12:30:32 +0000

    implemented simple submissions

--- tsp/views.py
+++ tsp/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from .models import Submission
 
 def home(request):
     submissions = Submission.objects.all()
     return render(request, 'home.html', {'submissions': submissions})
+
+@csrf_exempt
+def submit(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        score = request.POST.get('score')
+
+        submission = Submission(name=name, score=score)
+        submission.save()
+
+        return redirect('home')
+
+    return render(request, 'submit.html')
